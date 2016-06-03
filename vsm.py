44bcdf3974be079339653b6e8d01110bba0ba9cd
hzfGuardian <412571747@qq.com>
2016-06-03 10:20:17 +0800

    我也不知道这文件在干嘛

--- vsm.py
+++ vsm.py
@@ -3,14 +3,16 @@
 import os
 import glob
 import math
+import dict_op
 
+"""
 file_path = []
 
 for fn in glob.glob('Reuters/' + os.sep + '*.html'):
     file_path += [fn]
 
 print len(file_path)
-
+"""
 
 # for root, dirs, files in os.walk('Reuters/'):
 #    print("Root = ", root, "dirs = ", dirs, "files = ", files)
