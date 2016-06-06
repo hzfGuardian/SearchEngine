2cb2049d38dc4057f1e32c777593b1ee0ef0849d
hzfGuardian <412571747@qq.com>
2016-06-06 22:42:49 +0800

    merge all documents into one file

---
+++ merge_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import glob
+import os
+
+fo = open("trainer", "w")
+
+for fn in glob.glob('Reuters/' + os.sep + '*.html'):
+        file_object = open(fn)
+        all_the_text = file_object.read()
+        fo.write(all_the_text)
+        file_object.close()
+
+fo.close()
+
